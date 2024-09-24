9a9e00daf9bf1321bb1b8ed8ec102be4eaed982c
christalselvin <selvin472001@gmail.com>
2024-09-25 00:24:52 +0530

    first commit

---
+++ config.py
@@ -0,0 +1,7 @@
+# Your Bot token in the quotes taken from @BotFather
+BOT_TOKEN = "7117936584:AAGOax3YoEppDqSj1UCAEZ0OiEZ5HwLPiew"
+# Project Name of your Project
+PROJECT_NAME = "Lolitta"
+
+# If you forked the source code, grab your repo's link and paste it below!
+GIT_REPO_URL = "https://github.com/MadeByThePinsHub/GitGram"
